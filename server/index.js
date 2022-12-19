439b5618f3f39053471d7947ecbd4487195ebc9e
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2022-12-19 12:54:31 +0200

    start creating routes

--- server/index.js
+++ server/index.js
@@ -1,2 +1,15 @@
 const express = require("express");
 require("dotenv").config();
+const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    exposedHeaders: "x-auth-token",
+    methods: "GET,PUT,POST,DELETE",
+  })
+);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server Start At: " + process.env.PORT);
+});

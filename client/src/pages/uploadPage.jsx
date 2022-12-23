1ada6591ad491d9094e19e2c807280579211b02b
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2022-12-23 13:29:39 +0200

    Countine Bulding The Client Side

--- client/src/pages/uploadPage.jsx
+++ client/src/pages/uploadPage.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Form } from "react-router-dom";
 import NavBar from "../elements/navBar";
+import "../css/upload.css";
 
 const UploadPage = () => {
   return (
-    <div>
+    <div className="uploadPage">
       <NavBar />
+      <div className="upload">
+        <Form>
+          <input
+            type="text"
+            placeholder="Enter Song Name"
+            className="input input--big"
+          />
+          <div className="upload-cover">
+            <input type="file" accept="audio/*" className="input-file" />
+          </div>
+          <div className="upload-cover">
+            <input type="file" accept="image/*" className="input-file" />
+          </div>
+        </Form>
+      </div>
     </div>
   );
 };

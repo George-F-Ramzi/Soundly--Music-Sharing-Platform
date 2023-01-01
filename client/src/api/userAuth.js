096e0f74ab6470bd5c66866312b731ee13a05d32
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-01-01 11:02:10 +0200

    Delpying Client

--- client/src/api/userAuth.js
+++ client/src/api/userAuth.js
@@ -1,19 +1,11 @@
 import axios from "axios";
 
 export const LoginApi = async (data) => {
-  const loginApi = "http://localhost:3999/login";
+  const loginApi = "https://soundly.onrender.com/login";
   return await axios.post(loginApi, data);
 };
 
 export const RegisterApi = async (data) => {
-  const registerApi = "http://localhost:3999/register";
+  const registerApi = "https://soundly.onrender.com/register";
   return await axios.post(registerApi, data);
-};
-
-export const AllowJoin = async () => {
-  const token = localStorage.getItem("token");
-  const AllowJoin = "http://localhost:3999/allow";
-  return await axios.get(AllowJoin, {
-    headers: { "x-auth-token": token },
-  });
 };

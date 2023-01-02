481ff77b36c10e90c8d02cdccff01657d5700e6a
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-01-02 12:40:48 +0200

    Finishing Client

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import "../css/landingPage.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfSWQiOjQ2LCJpYXQiOjE2NzI2NTU5NjN9.L3--b-OvkWmEPbkUS3TlinjFay_y0s6aDIcAqxEu5nA";
+
   return (
     <div className="landing-page">
       <h1 className="landing__title">
         A New Sharing Experience With Soundly Share Explore And Much More
       </h1>
       <Outlet />
+      <button
+        onClick={() => {
+          localStorage.setItem("token", token);
+          navigate("/home");
+        }}
+        className="btn btn--small body1 demo"
+      >
+        Login As Demo
+      </button>
     </div>
   );
 };

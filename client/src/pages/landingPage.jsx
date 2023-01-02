f0e59b4430a59ff02fbeab393081a6e1b9720650
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-01-02 13:57:32 +0200

    Client Auto Login ROute

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/landingPage.css";
 import { Outlet, useNavigate } from "react-router-dom";
 import { AllowJoin } from "../api/userAuth";
@@ -6,6 +6,8 @@ import { toast } from "react-toastify";
 
 const LandingPage = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfSWQiOjQ2LCJpYXQiOjE2NzI2NTU5NjN9.L3--b-OvkWmEPbkUS3TlinjFay_y0s6aDIcAqxEu5nA";
 
@@ -16,28 +18,37 @@ const LandingPage = () => {
   const autoLogin = async () => {
     if (!localStorage.getItem("token")) return;
     try {
+      setLoading(true);
       await AllowJoin();
+      setLoading(false);
       return navigate("/home");
     } catch (error) {
+      setLoading(false);
       return toast("Login Session Expired", { type: "error" });
     }
   };
 
   return (
     <div className="landing-page">
-      <h1 className="landing__title">
-        A New Sharing Experience With Soundly Share Explore And Much More
-      </h1>
-      <Outlet />
-      <button
-        onClick={() => {
-          localStorage.setItem("token", token);
-          navigate("/home");
-        }}
-        className="btn btn--small body1 demo"
-      >
-        Login As Demo
-      </button>
+      {loading ? (
+        ""
+      ) : (
+        <>
+          <h1 className="landing__title">
+            A New Sharing Experience With Soundly Share Explore And Much More
+          </h1>
+          <Outlet />
+          <button
+            onClick={() => {
+              localStorage.setItem("token", token);
+              navigate("/home");
+            }}
+            className="btn btn--small body1 demo"
+          >
+            Login As Demo
+          </button>
+        </>
+      )}
     </div>
   );
 };

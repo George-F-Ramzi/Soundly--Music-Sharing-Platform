3c64101465797381dfe8692a21418893db975bd7
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-04-09 13:13:05 +0200

    not found page redesign done

--- client/src/pages/notFound.tsx
+++ client/src/pages/notFound.tsx
@@ -1,19 +1,21 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
   const navigate = useNavigate();
   return (
-    <div className="notfound">
-      <h1 className="not-title">404</h1>
-      <h5 className="not-subtitle">
-        sorry the content you were trying to reach was not found
-      </h5>
+    <div className="w-full p-8 tablet:p-2  h-full flex-col flex items-center justify-center text-white">
+      <h1 className="text-[156px] tablet:text-[48px] phone:text-[36px] leading-[150%] font-bold">
+        4<span className="text-emerald-400">0</span>4
+      </h1>
+      <h1 className="leading-[150%] tablet:text-[36px] phone:text-[20px] text-center font-bold text-[64px]">
+        SORRY,THERE’S
+        <br /> NOTHING TO SHOW HERE
+      </h1>
       <button
-        onClick={() => navigate("../", { replace: true })}
-        className="btn btn--small body1"
+        onClick={() => navigate("/home")}
+        className="mt-[48px] text-black tablet:px-4 tablet:text-sm text-lg tablet:py-2 font-bold  px-[48px] py-[12px] bg-gradient1 rounded-full"
       >
-        Return
+        GO HOME
       </button>
     </div>
   );
